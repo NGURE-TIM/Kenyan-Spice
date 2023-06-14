1840e440a10ca521a8e71d9b94884622b54ef721
NGURE-TIM <kangushtim@gmail.com>
2023-06-14 23:26:44 +0300

    dasboard works

--- lib/Dashboard/database/databasehelper.dart
+++ lib/Dashboard/database/databasehelper.dart
@@ -1,57 +1,66 @@
-import 'dart:io';
-import 'package:path/path.dart' as path;
-import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
-import 'package:flutter/services.dart' show rootBundle;
+import 'package:path/path.dart';
+import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
 
-Future<Database> createDatabase() async {
-  final databasePath = await getDatabasesPath();
-  final path = join(databasePath, 'recipes.db');
+class DatabaseHelper {
+  static DatabaseHelper? _instance;
+  factory DatabaseHelper() {
+    if (_instance == null) {
+      _instance = DatabaseHelper._();
+    }
+    return _instance!;
+  }
 
-  return await openDatabase(
-    path,
-    version: 1,
-    onCreate: (db, version) async {
-      await db.execute('''
-        CREATE TABLE recipes (
-          id INTEGER PRIMARY KEY AUTOINCREMENT,
-          name TEXT,
-          ingredients TEXT,
-          instructions TEXT
-        )
-      ''');
-    },
-  );
+  static Database? _database;
+
+  Future<Database?> get database async {
+    if (_database != null) {
+      return _database;
+    }
+    _database = await initDatabase();
+    return _database;
+  }
+
+  DatabaseHelper._();
+
+  Future<Database?> initDatabase() async {
+    String databasesPath = await getDatabasesPath();
+    String databasePath = join(databasesPath, 'recipes.db');
+    bool databaseExists = await checkIfDatabaseExists(databasePath);
+
+    if (!databaseExists) {
+      await copyDatabaseFromAssets(databasePath);
+    }
+
+    return await openDatabase(databasePath);
+  }
+
+  Future<bool> checkIfDatabaseExists(String databasePath) async {
+    return databaseFactory.databaseExists(databasePath);
+  }
+
+  Future<void> copyDatabaseFromAssets(String databasePath) async {
+    var documentsDirectory = await getApplicationDocumentsDirectory();
+    String destPath = join(documentsDirectory.path, 'recipes.db');
+
+    ByteData? databaseByteData;
+    try {
+      databaseByteData = await rootBundle.load('database_assests/recipes.db');
+    } catch (e) {
+      throw Exception('Failed to load prepopulated database file: $e');
+    }
+
+    try {
+      final bytes = databaseByteData!.buffer.asUint8List();
+      await File(destPath).writeAsBytes(bytes, flush: true);
+    } catch (e) {
+      throw Exception('Failed to write prepopulated database to path: $e');
+    }
+  }
 }
 
-void insertRecipe(Database database, String name, String ingredients, String instructions) async {
-  await database.insert(
-    'recipes',
-    {
-      'name': name,
-      'ingredients': ingredients,
-      'instructions': instructions,
-    },
-  );
-}
-
-Future<List<Map<String, dynamic>>> getRecipes(Database database) async {
-  return await database.query('recipes');
-}
-void main() async {
-  final database = await createDatabase();
-
-  // Insert a recipe
-  insertRecipe(database, 'Spaghetti Bolognese', 'Ingredients...', 'Instructions...');
-
-  // Query all recipes
-  final recipes = await getRecipes(database);
-  recipes.forEach((recipe) {
-    print('Name: ${recipe['name']}');
-    print('Ingredients: ${recipe['ingredients']}');
-    print('Instructions: ${recipe['instructions']}');
-  });
-}
 
 
 
